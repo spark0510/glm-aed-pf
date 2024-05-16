9ad25a1cd84a7d49deef1c472060271c1bb59419
rqthomas <rqthomas@vt.edu>
2024-05-16 15:37:59 -0400

    ensure model to data unit conversion is consistent

--- run_glm_aed_pf.R
+++ run_glm_aed_pf.R
@@ -38,6 +38,14 @@ combine_ensembles(ensemble_path = ensemble_path, combined_path = combined_path)
 
 
 # Standardize obs and ensemble output
+states_config <- read_csv(config$states_config_file, show_col_types = FALSE)
+
+oxy <- states_config |> 
+  filter(state_names == "OXY_oxy")
+
+fdom <- states_config |> 
+  filter(state_names == "OGM_doc")
+
 prior <- arrow::open_dataset(combined_path) |>
   mutate(variable = ifelse(variable == "temp", "Temp_C_mean", variable),
          variable = ifelse(variable == "PHY_tchla", "Chla_ugL_mean", variable),
@@ -45,12 +53,12 @@ prior <- arrow::open_dataset(combined_path) |>
          variable = ifelse(variable == "secchi", "Secchi_m_sample", variable),
          variable = ifelse(variable == "CAR_ch4_atm", "ch4flux_umolm2s_mean", variable),
          variable = ifelse(variable == "co2_flux", "co2flux_umolm2s_mean", variable)) |>
-  mutate(prediction = ifelse(variable == "DO_mgL_mean", prediction/1000*(32),prediction),
+  mutate(prediction = ifelse(variable == "DO_mgL_mean", prediction / oxy$conversion_slope,prediction),
          prediction = ifelse(variable == "ch4flux_umolm2s_mean", (prediction * 1000) / (60 * 60 * 24),prediction),
          prediction = ifelse(variable == "co2flux_umolm2s_mean", (prediction * 1000) / (60 * 60 * 24),prediction)) |>
   collect() |>
   pivot_wider(names_from = variable, values_from = prediction) |>
-  mutate(fDOM_QSU_mean = (OGM_doc + OGM_docr + 151.3407) / 29.62654)  |>
+  mutate(fDOM_QSU_mean = (OGM_doc + OGM_docr + fdom$conversion_intercept) / fdom$conversion_slope)  |>
   select(-OGM_doc, -OGM_docr) |>
   pivot_longer(-c("depth_m", "datetime",  "ensemble"), names_to = "variable", values_to = "prediction") |>
   filter(!is.na(prediction))
